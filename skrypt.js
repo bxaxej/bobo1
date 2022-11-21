4aa0488a3c3a5b60145ab2b506bf1c7f07dfc2d6
kamil <kam@mail.com>
2022-11-21 10:40:33 +0100

    'lol'

--- skrypt.js
+++ skrypt.js
@@ -11,9 +11,9 @@ function jump(){
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+    if(blockLeft<40 && blockLeft>-40 && characterTop>=400){
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
+        alert("Koniec gry. Diamenty: "+Math.floor(counter/100));
         counter=0;
         block.style.animation = "block 1s infinite linear";
     }else{
